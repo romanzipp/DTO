8d60bc9964b3d0b08b220a173a398ef0d803c645
Roman Zipp <ich@ich.wtf>
2020-10-23 08:56:58 +0200

    Declare strict types for all classes

--- src/Cases/AbstractCase.php
+++ src/Cases/AbstractCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace romanzipp\DTO\Cases;
 
 abstract class AbstractCase
--- src/Cases/PascalCase.php
+++ src/Cases/PascalCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace romanzipp\DTO\Cases;
 
 class PascalCase extends AbstractCase
