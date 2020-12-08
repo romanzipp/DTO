0a67538a6e056cf9df6a249a24c5a3bf6632f57f
Roman Zipp <ich@ich.wtf>
2020-12-08 15:37:55 +0100

    Move flexible static property to attribute

--- tests/FlexibleValuesTest.php
+++ tests/FlexibleValuesTest.php
@@ -3,41 +3,43 @@
 namespace romanzipp\DTO\Tests;
 
 use romanzipp\DTO\AbstractData;
+use romanzipp\DTO\Attributes\Flexible;
 use romanzipp\DTO\Exceptions\InvalidDataException;
+use romanzipp\DTO\Tests\Support\FlexibleData;
 
 class FlexibleValuesTest extends TestCase
 {
+    public function testClassIsDetectedAsFlexible()
+    {
+        self::assertTrue(
+            FlexibleData::isFlexible()
+        );
+    }
+
     public function testNotFlexibleFailing()
     {
         $this->expectException(InvalidDataException::class);
 
-        new class(['foo' => 'bar']) extends AbstractData {
-            protected static bool $flexible = false;
-        };
+        new class(['foo' => 'bar']) extends AbstractData {};
     }
 
     public function testNotFlexibleFailingMultiple()
     {
         $this->expectException(InvalidDataException::class);
 
-        new class(['foo' => 'bar', 'bar' => 'foo']) extends AbstractData {
-            protected static bool $flexible = false;
-        };
+        new class(['foo' => 'bar', 'bar' => 'foo']) extends AbstractData {};
     }
 
     public function testOverloading()
     {
-        $data = new class(['foo' => 'bar']) extends AbstractData {
-            protected static bool $flexible = true;
-        };
+        $data = new #[Flexible] class(['foo' => 'bar']) extends AbstractData {};
 
         self::assertSame('bar', $data->foo);
     }
 
     public function testOverloadingWithExisting()
     {
-        $data = new class(['foo' => 'bar', 'bar' => 'foo']) extends AbstractData {
-            protected static bool $flexible = true;
+        $data = new #[Flexible] class(['foo' => 'bar', 'bar' => 'foo']) extends AbstractData {
             public string $bar;
         };
 
---
+++ tests/Support/FlexibleData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace romanzipp\DTO\Tests\Support;
+
+use romanzipp\DTO\AbstractData;
+use romanzipp\DTO\Attributes\Flexible;
+
+#[Flexible]
+class FlexibleData extends AbstractData
+{
+
+}
